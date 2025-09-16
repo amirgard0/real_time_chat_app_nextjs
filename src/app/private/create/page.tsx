de8c0436c7faceae3021fdb1f6c859b0c9dc2431
amirgard0 <amirgard0@gmail.com>
2025-09-16 21:31:03 +0330

    fully changed the private/create page

--- src/app/private/create/page.tsx
+++ src/app/private/create/page.tsx
@@ -1,41 +1,51 @@
-"use server"
+import { prisma } from "@/lib/prisma"
+import SearchForm from "./SearchForm"
+import { Card, CardAction, CardContent, CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { auth } from "@/lib/auth"
+import { handleChatAction } from "./actions"
 
-import { auth } from "@/lib/auth";
-import { prisma } from "@/lib/prisma";
-
-async function findOrCreatePrivateChat(user1Id: string, user2Id: string) {
-  "use server"
-  const sortedIds = [user1Id, user2Id].sort();
-
-  return await prisma.privateChat.upsert({
-    where: {
-      user1Id_user2Id: {
-        user1Id: sortedIds[0],
-        user2Id: sortedIds[1]
-      }
-    },
-    create: {
-      user1Id: sortedIds[0],
-      user2Id: sortedIds[1]
-    },
-    update: {}
-  });
-}
-
-function handleFormAction(formData: FormData) {
-
-}
-
-export default async () => {
-  const session = await auth()
-
-  if (!session) {
-    return <div>
-      not authenticated
-    </div>
+export default async ({ searchParams }: { searchParams: any }) => {
+  const awaitedSearchParams = await searchParams
+  let users
+  if (awaitedSearchParams.userId! && awaitedSearchParams.userId.length >= 5) {
+    users = await prisma.user.findMany({
+      where: { publicId: { contains: awaitedSearchParams.userId } }
+    })
   }
-
+  const session = await auth()
   return <div>
-    create
+    <SearchForm />
+    {
+      users && <div className="grid grid-cols-5 p-7">
+        {users.map((user) => {
+          return <Card key={user.id}>
+            <CardTitle className="px-3">
+              <h1>
+                {user.name} <small className="opacity-70">{user.publicId}</small>
+              </h1>
+            </CardTitle>
+            <CardContent>
+              <small>
+                lastSeen: {user.lastSeen?.toString() || "unknown"}
+              </small>
+              <br />
+              <small>
+                {user.isOnline ? "online" : "offline"}
+              </small>
+            </CardContent>
+            <CardAction className="w-full px-3 py-0">
+              <form action={handleChatAction}>
+                <input type="hidden" value={user.id} name="targetUserId" />
+                <input type="hidden" value={session?.user?.id} name="userId" />
+                <Button className="w-full" type="submit">
+                  chat
+                </Button>
+              </form>
+            </CardAction>
+          </Card>
+        })}
+      </div>
+    }
   </div>
 }
