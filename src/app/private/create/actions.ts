e96d6d289534090326e9b1a8c2773c4f9036f9eb
amirgard0 <amirgard0@gmail.com>
2025-09-16 21:30:40 +0330

    added action to private/create

---
+++ src/app/private/create/actions.ts
@@ -0,0 +1,38 @@
+"use server"
+import { prisma } from "@/lib/prisma";
+import { redirect, RedirectType } from "next/navigation";
+
+async function findOrCreatePrivateChat(user1Id: string, user2Id: string) {
+  const sortedIds = [user1Id, user2Id].sort();
+
+  return await prisma.privateChat.upsert({
+    where: {
+      user1Id_user2Id: {
+        user1Id: sortedIds[0],
+        user2Id: sortedIds[1]
+      }
+    },
+    create: {
+      user1Id: sortedIds[0],
+      user2Id: sortedIds[1]
+    },
+    update: {}
+  });
+}
+
+export async function handleChatAction(formData: FormData) {
+  const targetUserId = formData.get("targetUserId") as string
+  const userId = formData.get("userId") as string
+  if (!targetUserId || !userId) {
+    return
+  }
+  let chat
+  try {
+    chat = await findOrCreatePrivateChat(targetUserId, userId)
+  } catch (error) {
+    console.error()
+  }
+  if (chat) {
+    redirect("/private/chat/" + chat.id, RedirectType.replace)
+  }
+}
